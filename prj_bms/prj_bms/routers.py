ee6ac843729a068c9929045999db52e399c8bdc2
chandamaheshwari <23.chandamaheshwari@gmail.com>
2019-09-23 19:36:56 +0530

    working CRUD for invoice

--- prj_bms/prj_bms/routers.py
+++ prj_bms/prj_bms/routers.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from bms.viewsets import CategoryViewSet, BrandViewSet, ShopViewSet
+from bms.viewsets import CategoryViewSet, BrandViewSet, ShopViewSet, InvoiceViewSet
 
 router = routers.DefaultRouter()
 router.register(r'category', CategoryViewSet)
 router.register(r'brand', BrandViewSet)
 router.register(r'shop', ShopViewSet)
+router.register(r'invoice', InvoiceViewSet)
 
