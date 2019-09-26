f6cd9ea064d292618ca916d62a2b50c0cb114b5c
chandamaheshwari <23.chandamaheshwari@gmail.com>
2019-09-26 20:00:52 +0530

    fixed read and write serializers

--- prj_bms/bms/serializers.py
+++ prj_bms/bms/serializers.py
@@ -2,7 +2,11 @@ from .models import Category, Brand , Shop, Invoice, Quantity, Shift,BmsUser
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-
+class ReadCategorySerializer(serializers.HyperlinkedModelSerializer):
+    category_id = serializers.ReadOnlyField()
+    class Meta:
+        model = Category
+        fields = ['category_id','category_name','category_description']
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     category_id = serializers.ReadOnlyField()
@@ -11,15 +15,26 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
         fields = ['category_id','category_name','category_description']
 
 
-class BrandSerializer(serializers.HyperlinkedModelSerializer):
+class ReadBrandSerializer(serializers.HyperlinkedModelSerializer):
     brand_id = serializers.ReadOnlyField()
-    category_id = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Category.objects.all())
+    category_id = CategorySerializer()
     class Meta:
         model = Brand
         fields =  ['brand_id','brand_name','brand_description','category_id','brand_p_cost','brand_q_cost','brand_n_cost','brand_d_cost','brand_l_cost','brand_xg_cost','brand_y_cost','brand_p_sale','brand_q_sale','brand_n_sale','brand_d_sale','brand_l_sale','brand_xg_sale','brand_y_sale']
 
+class BrandSerializer(serializers.ModelSerializer):
+        brand_id = serializers.ReadOnlyField()
+        category_id = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Category.objects.all())
+        class Meta:
+            model = Brand
+            fields =  ['brand_id','brand_name','brand_description','category_id','brand_p_cost','brand_q_cost','brand_n_cost','brand_d_cost','brand_l_cost','brand_xg_cost','brand_y_cost','brand_p_sale','brand_q_sale','brand_n_sale','brand_d_sale','brand_l_sale','brand_xg_sale','brand_y_sale']
 
 class QuantitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Quantity
+        fields = ['quantity_name','quantity_bottles']
+
+class ReadQuantitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Quantity
         fields = ['quantity_name','quantity_bottles']
@@ -29,23 +44,33 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['username']
 
-class BmsUserSerializer(serializers.HyperlinkedModelSerializer):
+class ReadUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class BmsUserSerializer(serializers.ModelSerializer):
+    username = serializers.PrimaryKeyRelatedField(read_only=False, queryset=User.objects.all())
+    class Meta:
+            model = BmsUser
+            fields = ['username','user_id','user_first_name','user_last_name','user_role']
+
+class ReadBmsUserSerializer(serializers.HyperlinkedModelSerializer):
     username = UserSerializer()
     class Meta:
         model = BmsUser
         fields = ['username','user_id','user_first_name','user_last_name','user_role']
 
-    def create(self, validated_data):
-        users_data = validated_data.pop('username')
-        bms_user_data = BmsUser.objects.create(**validated_data)
-        for user_data in users_data:
-            User.objects.create(bms_user_data=bms_user_data, **user_data)
-        return bms_user_data
-
-
 class ShopSerializer(serializers.HyperlinkedModelSerializer):
     shop_id = serializers.ReadOnlyField()
     shop_admin = BmsUserSerializer()
+    class Meta:
+        model = Shop
+        fields =   ['shop_id','shop_name', 'shop_owner', 'shop_address','shop_admin']
+
+class ReadShopSerializer(serializers.HyperlinkedModelSerializer):
+    shop_id = serializers.ReadOnlyField()
+    shop_admin = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Shop.objects.all())
     class Meta:
         model = Shop
         fields =   ['shop_id','shop_name', 'shop_owner', 'shop_address','shop_admin']
@@ -58,11 +83,28 @@ class ShiftSerializer(serializers.HyperlinkedModelSerializer):
         model = Shift
         fields = ['brand_id','stock_shift_date','stock_shift_from','stock_shift_to','stock_shift_p','stock_shift_q','stock_shift_n','stock_shift_d','stock_shift_l','stock_shift_xg','stock_shift_y']
 
-class InvoiceSerializer(serializers.HyperlinkedModelSerializer):
+class ReadShiftSerializer(serializers.HyperlinkedModelSerializer):
+    brand_id =  serializers.PrimaryKeyRelatedField(read_only=False, queryset=Brand.objects.all())
+    stock_shift_to =  serializers.PrimaryKeyRelatedField(read_only=False, queryset=Shop.objects.all())
+    class Meta:
+        model = Shift
+        fields = ['brand_id','stock_shift_date','stock_shift_from','stock_shift_to','stock_shift_p','stock_shift_q','stock_shift_n','stock_shift_d','stock_shift_l','stock_shift_xg','stock_shift_y']
+
+
+class ReadInvoiceSerializer(serializers.HyperlinkedModelSerializer):
     invoice_transaction_id = serializers.ReadOnlyField()
     shop_id = ShopSerializer()
     brand_id = BrandSerializer()
     category_id = CategorySerializer()
+    class Meta:
+        model = Invoice
+        fields =   ['invoice_transaction_id','shop_id','brand_id','category_id','invoice_brand_size','invoice_brand_qty','invoice_rate_per_case','invoice_no_of_bottles','invoice_total']
+
+class InvoiceSerializer(serializers.HyperlinkedModelSerializer):
+    invoice_transaction_id = serializers.ReadOnlyField()
+    shop_id = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Shop.objects.all())
+    brand_id = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Brand.objects.all())
+    category_id = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Category.objects.all())
     class Meta:
         model = Invoice
         fields =   ['invoice_transaction_id','shop_id','brand_id','category_id','invoice_brand_size','invoice_brand_qty','invoice_rate_per_case','invoice_no_of_bottles','invoice_total']
